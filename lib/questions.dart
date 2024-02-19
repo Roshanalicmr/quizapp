9272c083f7d5df28719685eecf0e7cb353334694
Roshanalicmr <160301154+Roshanalicmr@users.noreply.github.com>
2024-02-19 17:04:50 +0530

    Initial commit

---
+++ lib/questions.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class Questions extends StatefulWidget {
+  const Questions({Key? key}) : super(key: key);
+
+  @override
+  State<Questions> createState() => _QuestionsState();
+}
+
+class _QuestionsState extends State<Questions> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        backgroundColor:Colors.redAccent,
+      ),
+    );
+  }
+}
+
